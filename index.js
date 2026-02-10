05bdcaff8e280600259e81f138c51c5fd0d3b344
ahmedtahleesh17-afk <ahmedtahleesh17@gmail.com>
2026-02-10 23:22:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const mysql = require('mysql2/promise');
 
-// ===================== CLIENT =====================
+// ===================== CLIENT ====================
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -336,3 +336,4 @@ if (!process.env.DISCORD_TOKEN) {
 }
 
 client.login(process.env.DISCORD_TOKEN);
+
