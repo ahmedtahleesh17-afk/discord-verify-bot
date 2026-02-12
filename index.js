9b0fd2744f9b78c4a9d1aaf090a32604530a8645
ahmedtahleesh17-afk <ahmedtahleesh17@gmail.com>
2026-02-12 14:29:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new Client({
   partials: ['CHANNEL']
 });
 
-// ===================== DATABASE =====================
+// ===================== DATABASE ====================
 const db = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
@@ -362,3 +362,4 @@ if (!process.env.DISCORD_TOKEN) {
 }
 
 client.login(process.env.DISCORD_TOKEN);
+
