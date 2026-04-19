9c2c02da4c8ba3698098aac099709d00a9c4726a
ahmedtahleesh17-afk <ahmedtahleesh17@gmail.com>
2026-04-19 12:56:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const transporter = nodemailer.createTransport({
   secure: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    pass: process.env.GMAIL_PASS
   }
 });
 
